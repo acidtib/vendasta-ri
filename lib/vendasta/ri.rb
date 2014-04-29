a3fdd0f313481815e1421257f1620e4c79cd9e36
Dan Vera <dan@yovu.co>
2014-04-29 15:11:50 -0400

    move stuff into blocks

--- lib/vendasta/ri.rb
+++ lib/vendasta/ri.rb
@@ -7,68 +7,110 @@ module Vendasta
   	:endpoint => 'https://reputation-intelligence-api.vendasta.com/api/v2'
 	}
 
-	module Ri
+	module RI
 
 		include HTTParty
 
-		def self.ipsum
-			puts "hi"
-			puts ENV["VENDASTA_RI_APIUSER"]
-			puts ENV["VENDASTA_RI_APIKEY"]
-  	end
+		## Accounts
+		module Accounts
 
-  	## Accounts
-  	def self.lookupAccounts # Lookup Accounts
-	    response = HTTParty.get("#{OPTIONS[:endpoint]}/account/lookup/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"]})
+			def self.ipsum
+				puts "hi"
+				puts ENV["VENDASTA_RI_APIUSER"]
+				puts ENV["VENDASTA_RI_APIKEY"]
+  		end
 
-	    if response.success?
-	    	response = JSON.parse(response.body)
-	    else
-	      response = JSON.parse(response.body)
-	    end
-	  end
+  		def self.lookupAccounts # Lookup Accounts
+		    response = HTTParty.get("#{OPTIONS[:endpoint]}/account/lookup/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"]})
 
-	  def self.getAccount(customerIdentifier) # Get Accounts by ID
-	    response = HTTParty.get("#{OPTIONS[:endpoint]}/account/get/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+		    if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
 
-	    if response.success?
-	    	response = JSON.parse(response.body)
-	    else
-	      response = JSON.parse(response.body)
-	    end
-	  end
+		  def self.getAccount(customerIdentifier) # Get Accounts by ID
+		    response = HTTParty.get("#{OPTIONS[:endpoint]}/account/get/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
 
+		    if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
+
+		end
 
 	  ## Visibility
-	  def self.lookupListings(customerIdentifier) # Lookup Listings
-	  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/lookupListings/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+	  module Visibility
 
-	  	if response.success?
-	    	response = JSON.parse(response.body)
-	    else
-	      response = JSON.parse(response.body)
-	    end
+	  	def self.lookupListings(customerIdentifier) # Lookup Listings
+		  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/lookupListings/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+
+		  	if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
+
+		  def self.lookupPossibleListings(customerIdentifier) # Lookup Possible Listings
+		  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/lookupPossibleListings/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+
+		  	if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
+
+		  def self.getVisibilityStats(customerIdentifier) # Get Visibility Stats
+		  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/getStats/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+
+		  	if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
+
 	  end
 
-	  def self.lookupPossibleListings(customerIdentifier) # Lookup Possible Listings
-	  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/lookupPossibleListings/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+	  ## Reviews
+	  module Reviews
 
-	  	if response.success?
-	    	response = JSON.parse(response.body)
-	    else
-	      response = JSON.parse(response.body)
-	    end
-	  end
+		  def self.getVisibilityStats(customerIdentifier, reviewId) # Get Reviews by ID
+		  	response = HTTParty.get("#{OPTIONS[:endpoint]}/review/get/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier, :reviewId => reviewId})
 
-	  def self.getVisibilityStats(customerIdentifier) # Get Visibility Stats
-	  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/getStats/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+		  	if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
 
-	  	if response.success?
-	    	response = JSON.parse(response.body)
-	    else
-	      response = JSON.parse(response.body)
-	    end
-	  end
+		  def self.searchReviews(customerIdentifier) # Search Reviews
+		  	response = HTTParty.get("#{OPTIONS[:endpoint]}/review/search/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+
+		  	if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
+
+		  def self.searchReviews(customerIdentifier) # Search Reviews
+		  	response = HTTParty.get("#{OPTIONS[:endpoint]}/review/search/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+
+		  	if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
+
+		end
 
   end
 end
