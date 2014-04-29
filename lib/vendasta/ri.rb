b24f583594a7ba741b92705a7eb3381339145647
Dan Vera <dan@ianalyst.com>
2014-04-29 13:24:11 -0400

    Lookup Accounts

--- lib/vendasta/ri.rb
+++ lib/vendasta/ri.rb
@@ -2,33 +2,35 @@ require "vendasta/ri/version"
 require "httparty"
 
 module Vendasta
+
+	OPTIONS = {
+  	:apiUser => 'IANA',
+  	:apiKey => 'sYMzAf3ZZm1wPMK0cKYJfXQDSpZJXwA6XaS7F3dz',
+  	:endpoint => 'https://reputation-intelligence-api.vendasta.com/api/v2'
+	}
+
 	module Ri
 
 		include HTTParty
 
-		
-
 		def self.ipsum
-			"hi"
+			puts "hi"
+			puts OPTIONS[:endpoint]
+			build_response = "#{OPTIONS[:endpoint]}/v2/account/lookup/"
+			puts build_response
   	end
 
   	def self.lookupAccounts
-	    response = HTTParty.get("https://reputation-intelligence-api.vendasta.com/api/v2/account/lookup/", :query => {:apiUser => "IANA", :apiKey => "sYMzAf3ZZm1wPMK0cKYJfXQDSpZJXwA6XaS7F3dz"})
+	    response = HTTParty.get("#{OPTIONS[:endpoint]}/account/lookup/", :query => {:apiUser => OPTIONS[:apiUser], :apiKey => OPTIONS[:apiKey]})
 
 	    if response.success?
 	    	response = JSON.parse(response.body)
 	    else
 	      # this just raises the net/http response that was raised
-	      puts "no"
+	      response = JSON.parse(response.body)
 	    end
 	  end
 
   end
 end
 
-
-#module Vendasta
-#  module Ri
-#    # Your code goes here...
-#  end
-#end
