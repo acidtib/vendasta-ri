69f510290e3974c09e168827abd8c67d915f12c5
Dan Vera <dan@yovu.co>
2014-04-29 14:55:27 -0400

    Get Visibility Stats

--- lib/vendasta/ri.rb
+++ lib/vendasta/ri.rb
@@ -59,6 +59,16 @@ module Vendasta
 	    end
 	  end
 
+	  def self.getVisibilityStats(customerIdentifier) # Get Visibility Stats
+	  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/getStats/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
+
+	  	if response.success?
+	    	response = JSON.parse(response.body)
+	    else
+	      response = JSON.parse(response.body)
+	    end
+	  end
+
   end
 end
 
