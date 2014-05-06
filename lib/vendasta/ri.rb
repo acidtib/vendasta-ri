9d6f23c1c876245cb35b6a2e62c161a2e73d947f
Dan Vera <dan@yovu.co>
2014-05-06 16:29:43 -0400

    look up listing by source

--- lib/vendasta/ri.rb
+++ lib/vendasta/ri.rb
@@ -55,6 +55,16 @@ module Vendasta
 		    end
 		  end
 
+		  def self.lookupListingsBysource(customerIdentifier, sourceid) # Lookup Listings by source id
+		  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/lookupListings/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier, :sourceId => sourceid})
+
+		  	if response.success?
+		    	response = JSON.parse(response.body)
+		    else
+		      response = JSON.parse(response.body)
+		    end
+		  end
+
 		  def self.lookupPossibleListings(customerIdentifier) # Lookup Possible Listings
 		  	response = HTTParty.get("#{OPTIONS[:endpoint]}/visibility/lookupPossibleListings/", :query => {:apiUser => ENV["VENDASTA_RI_APIUSER"], :apiKey => ENV["VENDASTA_RI_APIKEY"], :customerIdentifier => customerIdentifier})
 
